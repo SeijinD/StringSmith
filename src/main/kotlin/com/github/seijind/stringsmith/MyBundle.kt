1c704ae17b136838494d31723ce742834cfafc94
GitHub Action <action@github.com>
2026-06-07 08:59:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/seijind/stringsmith/MyBundle.kt
+++ src/main/kotlin/com/github/seijind/stringsmith/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.seijind.stringsmith
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/seijind/stringsmith/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/seijind/stringsmith/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.seijind.stringsmith.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
